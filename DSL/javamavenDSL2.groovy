49b265e36bbe412c5ce7f7c9d098525b224464f9
Luis Suclupe <lsuclupe@gmail.com>
2023-07-27 20:58:53 -0500

    Update javamavenDSL2.groovy

--- DSL/javamavenDSL2.groovy
+++ DSL/javamavenDSL2.groovy
@@ -1,18 +1,18 @@
 job('Java Maven App DSL 2') {
     description('Java Maven App con DSL para el curso de Jenkins')
     scm {
-        git('https://github.com/macloujulian/simple-java-maven-app.git', 'master') { node ->
-            node / gitConfigName('macloujulian')
-            node / gitConfigEmail('macloujulian@gmail.com')
+        git('https://github.com/lsuclupe01/simple-java-maven-app.git', 'main') { node ->
+            node / gitConfigName('lsuclupe01')
+            node / gitConfigEmail('lsuclupe@gmail.com')
         }
     }
     steps {
         maven {
-          mavenInstallation('mavenjenkins')
+          mavenInstallation('mavenJenkins')
           goals('-B -DskipTests clean package')
         }
         maven {
-          mavenInstallation('mavenjenkins')
+          mavenInstallation('mavenJenkins')
           goals('test')
         }
         shell('''
